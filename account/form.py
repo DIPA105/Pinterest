5a1f2412b30115301f12aa163af8f28e08c8bf81
Dipa Ghosh <79372059+DIPA105@users.noreply.github.com>
2021-06-22 19:07:40 +0530

    Add files via upload

---
+++ account/form.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.db.models import fields
+from .models import *
+from .models import UserDetail
+
+class AccountForm(forms.ModelForm):
+    
+    class Meta:
+        model = Account
+        fields = ['account_image'] 
+class UserUpdate(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username']
+class ProfileUpdate(forms.ModelForm):
+    class   Meta:
+        model = UserDetail
+        fields = ['email','image']
